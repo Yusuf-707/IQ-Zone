2ed62ec166ca3d8d18dc25415901664e373c9d03
Yusuf Kasimov <yusuf@example.com>
2026-05-31 14:10:53 +0600

    31.05.2026 update

--- boy years/app.js
+++ boy years/app.js
@@ -13,17 +13,21 @@ let kontrolBtn = document.querySelector(".kontrol-btn");
 let trueResult = document.querySelector(".true-result");
 let falseResult = document.querySelector(".false-result");
 //modal
-let modal = document.querySelector(".modal");
+let modal = document.querySelector(".modal-test");
 let answertest = document.querySelector(".answerTest");
 let trueText = document.querySelector(".truetext");
 let falseText = document.querySelector(".falsetext");
 // modal close
 let overlayClose = document.querySelector(".overlay");
 let CloseModalClose = document.querySelector(".fa-xmark");
-let CloseModalClose2 = document.querySelector(".btn-ok")
+let CloseModalClose2 = document.querySelector(".btn-ok");
 //footer
 let footer = document.querySelector("footer");
+let exitCount = 0;
+let minuta = 15;
+let sekunta = 0;
 
+let newData = new Date();
 const randomTest = () => {
   testFormes.forEach((testForm) => testForm.classList.add("hidden"));
   const mathRandom = Array.from(testFormes).sort(() => 0.5 - Math.random());
@@ -61,14 +65,41 @@ function hesapla() {
     answertest.textContent = "Нужно больше учиться.";
   }
   main.classList.add("hidden");
-  modal.classList.add("show");
+  modal.classList.remove("hidden");
   trueText.textContent = correct;
   falseText.textContent = falseCorrect;
+  let hour =
+    newData.getHours() < 10 ? "0" + newData.getHours() : newData.getHours();
+  let minut =
+    newData.getMinutes() < 10
+      ? "0" + newData.getMinutes()
+      : newData.getMinutes();
+  let date =
+    newData.getDate() < 10 ? "0" + newData.getDate() : newData.getDate();
+  let month = String(newData.getMonth() + 1).padStart(2, "0");
+  let year = newData.getFullYear();
+  let timeMinut = 14 - minuta;
+  let timeSecunt = 60 - sekunta;
+  let list = JSON.parse(localStorage.getItem("list")) || [];
+  list.push({
+    true: correct,
+    false: falseCorrect,
+    time: `${timeMinut < 10 ? "0" + timeMinut : timeMinut}:${timeSecunt < 10 ? "0" + timeSecunt : timeSecunt}`,
+    data: `${hour}:${minut}, ${date}.${month}.${year}`,
+  });
+  localStorage.setItem("list", JSON.stringify(list));
+  console.log(list);
 }
 // true & folse
 kontrolBtn.addEventListener("click", () => {
   hesapla();
   clearInterval(interval);
+  document.addEventListener("visibilitychange", () => {
+    exitCount = 0;
+  });
+  document.addEventListener("mouseleave", (e) => {
+    if (e.clientY <= 0) exitCount = 0;
+  });
 });
 
 //   Header  Menus
@@ -76,9 +107,6 @@ menu.addEventListener("click", () => {
   menus.classList.toggle("hidden");
 });
 //hour
-let minuta = 15;
-let sekunta = 0;
-
 let interval = setInterval(() => {
   if (sekunta === 0) {
     if (minuta === 0) {
@@ -94,7 +122,6 @@ let interval = setInterval(() => {
   secunt.textContent = String(sekunta).padStart(2, "0");
 }, 1000);
 // modal close
-let exitCount = 0;
 function HandleExit() {
   exitCount++;
   if (exitCount === 2) {
@@ -118,6 +145,6 @@ CloseModalClose.addEventListener("click", () => {
 });
 CloseModalClose2.addEventListener("click", () => {
   overlayClose.classList.add("hidden");
-})
+});
 //footer
 footer.addEventListener("click", hiddenMenus);
